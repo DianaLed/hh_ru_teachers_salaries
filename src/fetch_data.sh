1ecd5d090ac7835c40fa82260a2052e5e13bbd88
Rowan <rowanzen@student.21-school.ru>
2025-08-17 23:02:59 +0300

    Add script fetch_data

---
+++ src/fetch_data.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+curl -s "https://api.hh.ru/vacancies?per_page=20&text=teacher&area=66&search_field=name&professional_role=132" \
+| jq > ../data/raw/nnTeacher.json
+curl -s "https://api.hh.ru/vacancies?per_page=20&text=teacher&area=1&search_field=name&professional_role=132" \
+| jq > ../data/raw/moTeacher.json
