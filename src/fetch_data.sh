6d0a5c581e070a630ef446ef487b2d0dee894bdf
Rowan <rowanzen@student.21-school.ru>
2025-09-03 12:34:34 +0300

    99 per_page change

--- src/fetch_data.sh
+++ src/fetch_data.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 curl -s "https://api.hh.ru/vacancies?per_page=20&text=teacher&area=66&search_field=name&professional_role=132" \
 | jq > ../data/raw/nnTeacher.json
-curl -s "https://api.hh.ru/vacancies?per_page=20&text=teacher&area=1&search_field=name&professional_role=132" \
+curl -s "https://api.hh.ru/vacancies?per_page=99&text=teacher&area=1&search_field=name&professional_role=132" \
 | jq > ../data/raw/moTeacher.json
